e79f91de152c64c37aa0c4a4c9cc2abf4aa9b91e
Christian Stamati <christian.stamati@gmail.com>
2026-01-13 23:20:03 +0100

    remove eslint

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({
+  component: App,
+});
 
 function App() {
-  return <div className="font-bold text-2xl">My App</div>;
+  return <div className="font-bold text-2xl">My App stuff</div>;
 }
